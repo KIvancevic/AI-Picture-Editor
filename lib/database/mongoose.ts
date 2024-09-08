cada06847f1c10801d7ccc2c71775ac8116ee8f1
Kristian <kristianivancevic@gmail.com>
2024-09-08 18:54:10 +0200

    resolving eslint errors

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,16 +7,25 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+declare global {
+  var mongoose: MongooseConnection | undefined;
+}
+
+let cached: MongooseConnection = global.mongoose || {
+  conn: null,
+  promise: null,
+};
 
 if (!cached) {
-  cached = (global as any).mongoose = {
+  cached = {
     conn: null,
     promise: null,
   };
 }
 
-export const connectToDatabase = async () => {
+global.mongoose = cached;
+
+export const connectToDatabase = async (): Promise<Mongoose> => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
@@ -24,11 +33,10 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "imaginify",
+      dbName: "AI picture editor",
       bufferCommands: false,
     });
 
   cached.conn = await cached.promise;
-
   return cached.conn;
 };
